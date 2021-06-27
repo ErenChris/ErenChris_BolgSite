41cda9f5f53bc36057327148f925394c6b93327d
ErenChris <2485552681@qq.com>
2021-06-27 23:22:07 +0800

    Update

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using BolgSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BolgSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+       
+        [HttpGet]
+        public IActionResult GetEditResults(Post post)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
