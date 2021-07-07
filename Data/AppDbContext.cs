a11606caeb1761735c8aeda7debe5888fb9a4dbc
ErenChris <2485552681@qq.com>
2021-07-07 23:01:49 +0800

    Update

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,7 +9,7 @@ namespace BolgSite.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<DbContext> options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
